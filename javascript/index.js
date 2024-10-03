e2f729671f5118e7e20b83b13993a6929917c43a
Junio <junio237@hotmail.com>
2024-10-03 10:16:12 -0300

    atualições

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('collapsed');
+}
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
