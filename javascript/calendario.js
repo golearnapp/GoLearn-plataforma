3a5965e8df31835f474ae6b78d8d0f94a22b89a5
Junio <junio237@hotmail.com>
2024-10-21 09:16:13 -0300

    atualização

---
+++ javascript/calendario.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl,
+    {
+      initialView: 'dayGridMonth',
+      locale: 'pt-br',
+      headerToolbar: {
+        left: 'prev,today',
+        center: 'title',
+        right: 'next'}
+    });
+    calendar.render();
+});
